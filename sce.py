7fc51d6181db2065eeaf9986114c62bc377a3618
Josh Sjah <luckystripes60@gmail.com>
2020-07-16 20:26:40 -0700

    Arguments (#13)
    * add travis lint script
    
    * fixed filenotfound exception
    
    * fixed filenotfound exception
    
    * fixed filenotfound exception
    
    * fixed filenotfound exception
    
    * fixed filenotfound exception
    
    * added command line arguments
    
    * added arguments to command line

--- sce.py
+++ sce.py
@@ -1,10 +1,19 @@
+import argparse
 from tools.setup import SceSetupTool
 
 setup = SceSetupTool()
-setup.check_os()
-setup.check_docker()
-setup.check_mongo()
+parser = argparse.ArgumentParser()
 
-setup.setup_rpc()
-setup.setup_core_v4()
-setup.setup_discord_bot()
+parser.add_argument('--setup', dest='setup', action='store_true')
+args = parser.parse_args()
+
+if args.setup:
+    setup.check_os()
+    setup.check_docker()
+    setup.check_mongo()
+
+    setup.setup_rpc()
+    setup.setup_core_v4()
+    setup.setup_discord_bot()
+else:
+    print('did nothing wrong')
