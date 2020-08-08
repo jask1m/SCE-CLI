fe2f7ff10a2a1172f6fbcc504ef92356396b995b
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2020-08-07 20:05:52 -0700

    Fix gRPC Code Generation (#19)
    I accidentally deleted it in #15

--- sce.py
+++ sce.py
@@ -1,5 +1,6 @@
 import argparse
 from tools.setup import SceSetupTool
+from tools.sce_proto_generator import SceProtoGenerator
 from tools.sce_presubmit_handler import ScePresubmitHandler
 
 parser = argparse.ArgumentParser()
@@ -8,14 +9,23 @@ parser.add_argument(
     '(setup, run, presubmit, etc.)')
 parser.add_argument(
     '--project', '-p', nargs='+', help='Project to run presubmit checks for.')
+parser.add_argument(
+    '--proto', nargs=1,
+    help='The language(s) to generate proto code.')
+parser.add_argument(
+    '--language', nargs='+',
+    help='The language(s) to generate proto code.')
 args = parser.parse_args()
 
 if args.command == 'setup':
     setup = SceSetupTool()
     setup.setup()
-if args.command == 'presubmit':
+elif args.command == 'presubmit':
     test_project = ScePresubmitHandler(args.project)
     test_project.handle_testing()
-if args.command == 'test':
+elif args.command == 'generate':
+    generator = SceProtoGenerator(args.proto[0], args.language)
+    generator.handle_proto_generation()
+elif args.command == 'test':
     setup = SceSetupTool()
     setup.add_sce_alias()
