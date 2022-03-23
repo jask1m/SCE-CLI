5485fe1970ea25906f4afa03c6c27db3189cc4bf
andrewtedi <andrew.tedijanto@gmail.com>
2022-03-23 14:16:13 -0700

    Added additional handling for invalid project names

--- tools/sce_clone_handler.py
+++ tools/sce_clone_handler.py
@@ -19,9 +19,18 @@ class SceCloneHandler:
 		Valid projects are: {list(self.test_commands.keys())}.
 		""")
 
+	def invalidName(self):
+		print(f"""
+		{self.project[0]} is not a valid project name.
+		Valid projects are: {list(self.test_commands.keys())}.
+		""")
+
 	def handle_cloning(self):
-		if(not self.project or len(self.project) != 1 or self.project[0] not in self.projects):
+		if not self.project or len(self.project) != 1:
 			self.print_usage()
+			return
+		elif self.project[0] not in self.projects:
+			self.invalidName()
 			return
 		process = subprocess.Popen(['git', 'clone', self.projects[self.project[0].lower()]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 		stdout, stderr = process.communicate()
