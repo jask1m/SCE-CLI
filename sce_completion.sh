e308a92a722f9c640aa1469f7c81b6d99a956b58
gursheyss <rkwarya@gmail.com>
2024-03-05 02:39:19 -0800

    kinda fix ls

--- sce_completion.sh
+++ sce_completion.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
 _sce_completion() {
-    local cur prev opts
+    local cur prev opts repos
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts="clone run link setup completion"
     repos="Clark cleezy Quasar SCE-discord-bot SCEta"
 
-    if [[ ${prev} == "clone" || ${prev} == "run" || ${prev} == "link" ]] ; then
+    if [[ ${prev} == "clone" || ${prev} == "run" || ${prev} == "link" ]]; then
         COMPREPLY=( $(compgen -W "${repos}" -- ${cur}) )
-        if [[ ${#COMPREPLY[@]} -eq 0 ]]; then
-            COMPREPLY=( $(ls) )
-        fi
-        return 0
+    else
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     fi
 
-    if [[ ${cur} == * ]] ; then
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-        if [[ ${#COMPREPLY[@]} -eq 0 ]]; then
-            COMPREPLY=( $(ls) )
-        fi
-        return 0
+    if [[ ${#COMPREPLY[@]} -eq 0 ]]; then
+        COMPREPLY=( $(compgen -o default -- ${cur}) )
     fi
+
+    return 0
 }
 complete -F _sce_completion sce
